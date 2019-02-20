6223bbca84dc96d404dc810933bd771c36a17142
Julian Wolf <julianw@bbd.co.za>
2019-02-20 10:50:47 +0200

    Did stuff'

---
+++ typescript/greeter.js
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return "Hello " + person;
+}
+var user = "Jane Watson";
+console.log(~(1 << 31));
+console.log((1 << 31));
+//# sourceMappingURL=greeter.js.map
---
+++ typescript/greeter.ts
@@ -0,0 +1,10 @@
+function greeter<String>(person: String) {
+    return "Hello " + person;
+}
+
+var user: String = "Jane Watson";
+
+console.log(~(1 << 31));
+console.log((1 << 31));
+
+// 1 2 4 8
